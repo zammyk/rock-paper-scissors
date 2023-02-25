4c1f384be442e8e2a2b14093eaf1d1c0d67cf5f7
Rahul Hembrom <69256240+zammyk@users.noreply.github.com>
2023-02-25 21:36:09 +0530

    add GUI for rps

--- index.js
+++ index.js
@@ -9,17 +9,18 @@ function getComputerChoice() {
     return choices[choiceIndex];
 }
 
-function playRound(playerSelection,computerSelection) {
-    console.log("Player chose: "+ playerSelection +"\nComputer chose: "+ computerSelection);
+function playRound(playerSelection, computerSelection) {
+    let roundMessage = "";
     if(playerSelection == computerSelection) {
-        return drawMessage;
+        roundMessage+=drawMessage;
     }
-    if( (playerSelection == choices[0] && computerSelection == choices[2]) || 
+    else if( (playerSelection == choices[0] && computerSelection == choices[2]) || 
         (playerSelection == choices[1] && computerSelection == choices[0]) ||
         (playerSelection == choices[2] && computerSelection == choices[1])){
-        return winMessage;
+        roundMessage+=winMessage;
     }
-    return loseMessage;
+    else roundMessage+=loseMessage;
+    return roundMessage;
 }
 
 function isValidSelection(playerSelection) {
@@ -33,19 +34,49 @@ function isValidSelection(playerSelection) {
     return false;
 }
 
-// function game(){
-//     for(let i = 0;i <5;i++)
-//     {
-//         let playerSelection = prompt("Enter your choice:");
-//         if(isValidSelection(playerSelection) == false)
-//         {
-//             console.log(invalidMessage);
-//             continue;
-//         }
-//         playerSelection = playerSelection.toLowerCase();
-//         let computerSelection = getComputerChoice();
-//         console.log(playRound(playerSelection,computerSelection));
-//     }
-// }
+/*
+function game(){
+    for(let i = 0;i <5;i++)
+    {
+        let playerSelection = prompt("Enter your choice:");
+        if(isValidSelection(playerSelection) == false)
+        {
+            console.log(invalidMessage);
+            continue;
+        }
+        playerSelection = playerSelection.toLowerCase();
+        let computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection,computerSelection));
+    }
+}
 
-// game();
+game();
+*/
+
+function playRoundHelper(e){
+    let computerSelection = getComputerChoice();
+    let playerSelection;
+    if(this.id == 'rockBtn')
+        playerSelection = 'rock';
+    else if(this.id == 'paperBtn')
+        playerSelection = 'paper';
+    else if(this.id == 'scissorBtn')
+        playerSelection = 'scissor';
+    else{
+        console.log("Invalid Input!");
+        return;
+    }
+    let roundResultMessage = "Computer chose: "+computerSelection+". Round verdict: "+ playRound(playerSelection, computerSelection);
+    roundResult.textContent = roundResultMessage;
+}
+
+let rockBtn = document.getElementById('rockBtn');
+let paperBtn = document.getElementById('paperBtn');
+let scissorBtn = document.getElementById('scissorBtn');
+
+let roundResult = document.querySelector('.round-result');
+let score = document.querySelector('.score');
+
+rockBtn.addEventListener('click',playRoundHelper);
+paperBtn.addEventListener('click',playRoundHelper);
+scissorBtn.addEventListener('click',playRoundHelper);
